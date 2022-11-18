73fe980d0bb23dbba9a1bfaa454b4510f302d81f
mtaylor57 <matthew.taylor57@nhs.net>
2022-11-18 12:03:34 +0000

    add a server error

--- app.js
+++ app.js
@@ -56,7 +56,13 @@ app.use((err, req, res, next) => {
 app.use((err, req, res, next) => {
   if (err.code === "22P02") {
     res.status(400).send({ msg: "incorrect data type passed" });
+  } else {
+    next(err)
   }
 });
+app.use((err,req,res,next) => {
+  console.log(err)
+  res.status(500).send({msg: "Server error"})
+})
 
 module.exports = app;
